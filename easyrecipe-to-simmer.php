3d993dffcfc6eb65f6b3834abee6814bdfac80c4
JeffMatson <jeff@jeffmatson.net>
2016-07-20 22:29:08 -0400

    Initial Commit

---
+++ easyrecipe-to-simmer.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: EasyRecipe To Simmer
+Description: Migrates content from EasyRecipe to Simmer
+Version: 0.1
+Author: Jeff Matson
+Author URI: https://jeffmatson.net
+*/
+
+require_once( __DIR__ . '/autoloader.php' );
